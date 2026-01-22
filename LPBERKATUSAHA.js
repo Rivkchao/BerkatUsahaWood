421d0fca913b82041610d5e88a5663f53cada971
Rivkychao <rivky69ps@gmail.com>
2026-01-22 08:58:34 +0700

    detaildataproduct

--- LPBERKATUSAHA.js
+++ LPBERKATUSAHA.js
@@ -26,21 +26,30 @@ document.addEventListener('alpine:init', () => {
                 code: 'MM-JT-SC-01',
                 price: 'Mulai 2Jt-an',
                 img: 'img/mejascandinavian.jpg',
-                desc: 'Material Jati tua dengan finishing natural doff yang menonjolkan serat kayu asli.'
+                desc: 'Material Jati tua dengan finishing natural doff yang menonjolkan serat kayu asli.',
+                waktuPengerjaan: '7-10 hari kerja',
+                jenisBahan: 'Kayu Jati',
+                ukuran: '120x70x75 cm'
             },
             {
                 name: 'Kursi Kayu Ergonomis',
                 code: 'KU-ERG-01',
                 price: 'Rp 850.000',
                 img: 'img/kursikayuergonomis.jpg',
-                desc: 'Didesain khusus dengan sandaran melengkung untuk kenyamanan kerja jangka panjang.'
+                desc: 'Didesain khusus dengan sandaran melengkung untuk kenyamanan kerja jangka panjang.',
+                waktuPengerjaan: '5-7 hari kerja',
+                jenisBahan: 'Kayu Mahoni',
+                ukuran: '50x50x80 cm'
             },
             {
                 name: 'Lemari Minimalis 2 Pintu',
                 code: 'LM-MN-2P-01',
                 price: 'Rp 4.200.000',
                 img: 'img/lemariminimalis.jpg',
-                desc: 'Penyimpanan luas dengan sistem sliding door yang sangat hemat ruang.'
+                desc: 'Penyimpanan luas dengan sistem sliding door yang sangat hemat ruang.',
+                waktuPengerjaan: '10-14 hari kerja',
+                jenisBahan: 'Kayu Jati',
+                ukuran: '120x50x180 cm'
             }
         ]
     }));
